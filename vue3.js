21587540a08a28767ed272a9269d5ca076d6e482
Jin <10751888+SoraJin424@users.noreply.github.com>
2021-07-04 20:27:06 +0800

    📦 refactor: remove part of the configuration

--- vue3.js
+++ vue3.js
@@ -1,8 +1,5 @@
 module.exports = {
     parser: 'vue-eslint-parser',
-    parserOptions: {
-        parser: '@typescript-eslint/parser'
-    },
     extends: [ 'plugin:vue/vue3-recommended', 'eslint:recommended' ],
     rules: {
         'vue/singleline-html-element-content-newline': 'off'
