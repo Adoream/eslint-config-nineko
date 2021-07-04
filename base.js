a0d6e0e54a9046b6754b20558f858758cd23f227
Jin <10751888+SoraJin424@users.noreply.github.com>
2021-07-04 20:26:31 +0800

    🐛 fix: fix spelling errors

--- base.js
+++ base.js
@@ -5,7 +5,7 @@ module.exports = {
         ecmaFeatures: { jsx: true },
         ecmaVersion: 2019,
         sourceType: 'module',
-        equireConfigFile: false
+        requireConfigFile: false
     },
     env: {
         browser: true,
